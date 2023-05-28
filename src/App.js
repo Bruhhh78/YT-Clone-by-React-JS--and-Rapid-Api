91d8a6ad7c8cae3e7d00f9da7a291a56914107c9
Bruhhh78 <anmolsrivastava678@gmail.com>
2023-05-28 19:22:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App() {
+import Header from './Components/Header'
+import Feed from './Components/Feed'
+import SearchResults from './Components/SearchResult'
+import VideoDetails from './Components/VideoDetails'
+import { AppContext } from './Context/ContextAPI';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext>
+      <BrowserRouter>
+        <div className="flex flex-col h-full">
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route
+              path="/searchResult/:searchQuery"
+              element={<SearchResults />}
+            />
+            <Route path="/video/:id" element={<VideoDetails />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AppContext>
   );
 }
 
